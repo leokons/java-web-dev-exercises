1525a9d5f566927f63ead7b51b6c6643fb71eb71
Konstantin <phoenix05@gmail.com>
2020-01-07 05:29:45 -0600

    Done 2.0

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -7,18 +7,24 @@ public class Area {
     public static final double PI = 3.14;
 
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
-           Double radius = 0.0;
-           do {
-               System.out.println("Enter a radius: ");
-               radius = input.nextDouble();
-           } while (radius <= 0.0);
-            System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius) + ".");
-        } catch(Exception e) {
-            System.err.println("This isn't a number.");
-            //e.printStackTrace();
+        Scanner input = new Scanner(System.in);
+        boolean flag = true;
+        while (flag) {
+            try {
+                Double radius = 0.0;
+                do {
+                    System.out.println("Enter a radius: ");
+                    radius = input.nextDouble();
+                } while (radius <= 0.0);
+                System.out.println("The area of a circle of radius " + radius + " is: " + Circle.getArea(radius) + ".");
+                flag = false;
+            } catch (Exception e) {
+                System.err.println("This isn't a number.");
+                input.next();
+                //e.printStackTrace();
+            }
         }
-        //input.close();
+        input.close();
 
 
     }
